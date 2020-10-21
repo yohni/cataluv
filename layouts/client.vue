65dd0df030e8abc993d0a5ce7db6c75223f53ff9
yohni <yohni.123@gmail.com>
2020-10-22 06:36:05 +0700

    feat: 🎸 layout for detail and home pages

---
+++ layouts/client.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="client">
+    <Navbar />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/MyNavbar.vue'
+
+export default {
+  name: 'LayoutClient',
+  components: {
+    Navbar,
+  },
+}
+</script>
+
+<style></style>
---
+++ layouts/detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="client">
+    <Navbar />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/DetailNav.vue'
+
+export default {
+  name: 'LayoutClient',
+  components: {
+    Navbar,
+  },
+}
+</script>
+
+<style></style>

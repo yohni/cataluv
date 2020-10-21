ba34c0e12b109675a30ba2162066c112dbc016c4
yohni <yohni.123@gmail.com>
2020-10-22 06:35:14 +0700

    feat: 🎸 responsive container

---
+++ components/MyContainer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container container--mx-auto">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="sass" scoped>
+.container
+  &--mx-auto
+    max-width: 960px
+    margin: 0 auto
+</style>

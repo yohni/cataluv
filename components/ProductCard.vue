cb8dec39be3a7da0961e50b7a19763d8d8b7639b
yohni <yohni.123@gmail.com>
2020-10-22 06:35:39 +0700

    feat: 🎸 product card

---
+++ components/ProductCard.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="card">
+    <img :src="image" alt="picture" class="card__img" />
+    <div class="card__body">
+      <div class="card__body__info">
+        <div class="card__body__info__title">{{ name }}</div>
+        <div class="card__body__info__price">IDR {{ formatPrice(price) }}</div>
+      </div>
+      <div class="card__body__action">
+        <button
+          type="button"
+          class="card__body__action__fav fav"
+          @click="setFavorite"
+        >
+          <span
+            class="material-icons fav__icon"
+            :class="{ fav__icon__favorited: favorite }"
+            >{{ !favorite ? 'favorite_border' : 'favorite' }}</span
+          >
+        </button>
+        <button class="card__body__action__buy">Buy</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductCard',
+  props: ['image', 'name', 'price'],
+  data() {
+    return {
+      favorite: false,
+    }
+  },
+  methods: {
+    setFavorite() {
+      this.favorite = !this.favorite
+    },
+    formatPrice(value) {
+      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+    },
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+@import '@/assets/sass/variables'
+
+.card
+  background: $originWhite
+  max-width: 375px
+  width: 100%
+  box-shadow: 0px 4px 8px rgba(133, 133, 133, 0.25)
+  cursor: pointer
+  transition: all .25s
+  margin-bottom: 12px
+  border-radius: 5px
+  overflow: hidden
+
+  &:hover
+    box-shadow: 0px 8px 10px rgba(133, 133, 133, 0.25)
+
+  &__img
+    width: 375px
+    height: 375px
+
+  &__body
+    padding: 16px
+    display: flex
+    justify-content: space-between
+    align-items: center
+    font-family: Poppins, sans-serif
+    font-size: 16px
+
+    &__info
+      display: flex
+      flex-direction: column
+      color: $turnBlack
+
+      &__title
+        font-weight: 600
+
+      &__price
+        font-weight: 500
+
+    &__action
+      display: flex
+      align-items: center
+
+      .fav
+        margin-right: 14px
+        border: none
+        outline: none
+        background: none
+        cursor: pointer
+
+        &__icon
+          color: $smoothGrey
+          transition: all .2s
+
+          &__favorited
+            color: $giantPink
+      &__buy
+        border: none
+        outline: none
+        padding: 12px 32px
+        background: $giantPink
+        font-weight: 600
+        border-radius: 3px
+        color: $originWhite
+        cursor: pointer
+        transition: all .2s
+
+        &:hover
+          transform: translateY(-1px)
+</style>

361eddb8dcf9ee18f8f5cf218f04229ef83205b3
yohni <yohni.123@gmail.com>
2020-10-22 06:32:45 +0700

    feat: 🎸 favorite button

---
+++ components/ButtonFav.vue
@@ -0,0 +1,41 @@
+<template>
+  <button type="button" class="fav" @click="setFavorite">
+    <span
+      class="material-icons fav__icon"
+      :class="{ fav__icon__favorited: favorite }"
+      >{{ !favorite ? 'favorite_border' : 'favorite' }}</span
+    >
+  </button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      favorite: false,
+    }
+  },
+  methods: {
+    setFavorite() {
+      this.favorite = !this.favorite
+    },
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+@import '@/assets/sass/variables'
+.fav
+  margin-right: 14px
+  border: none
+  outline: none
+  background: none
+  cursor: pointer
+
+  &__icon
+    color: $smoothGrey
+    transition: all .2s
+
+    &__favorited
+      color: $giantPink
+</style>

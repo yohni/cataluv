42d60a89fa739031fb68da2d8dcc665b25150b4a
yohni <yohni.123@gmail.com>
2020-10-22 06:36:32 +0700

    feat: 🎸 heading component

---
+++ components/SectionHeading.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="heading">
+    <h2 class="heading__h2">
+      <slot />
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Heading',
+}
+</script>
+
+<style lang="sass" scoped>
+@import '@/assets/sass/variables'
+
+.heading
+  &__h2
+    font-size: 16px
+    font-family: Poppins, sans-serif
+    font-weight: 600
+    color: $originGrey
+    margin-bottom: 12px
+</style>

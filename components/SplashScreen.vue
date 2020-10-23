663532c2cbc6e2af8439aabc57a9fe42cfc8d593
yohni <yohni.123@gmail.com>
2020-10-23 08:42:35 +0700

    feat: 🎸 loading page

---
+++ components/SplashScreen.vue
@@ -0,0 +1,55 @@
+<template>
+  <Container class="wrapper">
+    <div class="splash">
+      <div class="splash__logo">
+        <Logo />
+      </div>
+      <div class="splash__text">Loading...</div>
+    </div>
+  </Container>
+</template>
+
+<script>
+import Logo from '@/components/LogoIcon.vue'
+import Container from '@/components/MyContainer.vue'
+
+export default {
+  name: 'SplashScreen',
+  components: {
+    Logo,
+    Container,
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+@import '@/assets/sass/variables'
+@include keyframes(clinging)
+  from
+    opacity: 1
+
+  to
+   opacity: 0
+.wrapper
+  position: relative
+  min-height: calc( 100vh - 80px )
+.splash
+  position: absolute
+  top: -80px
+  right: 0
+  bottom: 0
+  left: 0
+  background: $originWhite
+  display: flex
+  align-items: center
+  justify-content: center
+  flex-direction: column
+
+  &__logo
+    margin-bottom: 12px
+
+  &__text
+    color: $originGrey
+    font-family: Poppins, sans-serif
+    @include animation(clinging)
+</style>
